0e6a34ef3e2f3a2086ab17d12f7a2e070e9bfbe9
vinodhbasavani <vinodh2308@gmail.com>
2016-03-31 19:34:40 +0530

    Update Databaseconnection.java

--- src/com/netenrich/Databaseconnection.java
+++ src/com/netenrich/Databaseconnection.java
@@ -8,18 +8,19 @@ public class Databaseconnection {
 	private String username;
 	private String password;
 	private String database_url;
-	java.sql.Connection connection=null;
+	
 	
 	public Databaseconnection(String user_name,String Password,String databaseurl){
-		username=user_name;
-		password=Password;
-		database_url = databaseurl;
+		this.username=user_name;
+		this.password=Password;
+		this.database_url = databaseurl;
 	}
 	public Connection Access_Database(){
+		java.sql.Connection connection=null;
 		try {
 			
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			connection = DriverManager.getConnection(database_url,username,password);
+			 connection = DriverManager.getConnection(database_url,username,password);
 			
 		
 		} catch (SQLException e) {
