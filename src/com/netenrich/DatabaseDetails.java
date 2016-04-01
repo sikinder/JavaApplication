b7830c51f20d3e462dc519c54d3eda7677841583
govind45 <govindarajulu920@gmail.com>
2016-04-01 16:01:31 +0530

    Update DatabaseDetails.java

--- src/com/netenrich/DatabaseDetails.java
+++ src/com/netenrich/DatabaseDetails.java
@@ -2,7 +2,7 @@ package com.netenrich;
 
 public class DatabaseDetails {
 	private String database_url = "jdbc:mysql://databaseinstance.csganqaov5mj.us-east-1.rds.amazonaws.com:3306/register";
-	private String database_username = "root";
+	private String database_username = "roo";
 	private String database_password = "zxcv12345";
 	public String Databaseurl(){
 		return database_url;
