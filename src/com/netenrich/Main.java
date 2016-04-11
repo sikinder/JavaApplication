3f12a2d675a6508d9921a8d6c3a143d5df9fe225
vinodhbasavani <vinodh2308@gmail.com>
2016-04-11 14:12:08 +0530

    Update Main.java

--- src/com/netenrich/Main.java
+++ src/com/netenrich/Main.java
@@ -4,7 +4,8 @@ import java.sql.*;
 public class Main {
 	private static Scanner input;
 	public static void main(String args[]) throws SQLException{
-		String ver = MyClass.class.getPackage().getImplementationVersion();
+		String ver = com.netenrich.Main.class.getPackage().getImplementationVersion();
+		System.out.println(ver);
 		input = new Scanner(System.in);
 		System.out.println("1.Register");
 		System.out.println("2.Login");
