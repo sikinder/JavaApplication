5caeb200cd4c034b3517c145f37ddb783a337997
vinodhbasavani <vinodh2308@gmail.com>
2017-03-22 12:47:04 +0530

    Update DatabaseDetails.java

--- src/com/netenrich/DatabaseDetails.java
+++ src/com/netenrich/DatabaseDetails.java
@@ -3,7 +3,7 @@ package com.netenrich;
 public class DatabaseDetails {
 	private String database_url = "jdbc:mysql://localhost/register";
 	private String database_username = "root";
-	private String database_password = "root";
+	private String database_password = "root123";
 	public String Databaseurl(){
 		return database_url;
 		
